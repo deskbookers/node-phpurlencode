ea43045a82cd3388aee12185036910bb1def4ff9
chicocode <eu@chicocode.io>
2018-02-23 09:23:25 +0100

    fix: Adds compilation to be compatible with IE10

---
+++ dist/src/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const encode = function (str) {
+  return encodeURIComponent(str + '').replace(/[!'()]/g, escape).replace(/\*/g, '%2A').replace(/\%20/g, '+').replace(/~/g, '%7E');
+};
+
+const decode = function (str) {
+  return decodeURIComponent((str + '').replace(/%(?![\da-f]{2})/gi, function () {
+    return '%25';
+  }).replace(/\+/g, '%20'));
+};
+
+const definePrototypes = function () {
+  if (!String.prototype.urlencode) {
+    global.String.prototype.urlencode = function () {
+      return encode(this);
+    };
+  }
+  if (!String.prototype.urldecode) {
+    global.String.prototype.urldecode = function () {
+      return decode(this);
+    };
+  }
+};
+
+module.exports = encode;
+module.exports.encode = encode;
+module.exports.decode = decode;
+module.exports.definePrototypes = definePrototypes;
